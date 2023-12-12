6759f0c493c22075183ed980fdb12a071a1f625f
Fadel Firmansyah <fadelfirmansyah97@gmail.com>
2023-12-12 16:13:51 +0700

    chore: add  k8s manifest & ingress with letsencrypt
    a

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
-const PORT = 8000 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 
 app.get('/welcome/:nama?', (req, res) => {
   const nama = req.params.nama || 'Anonymous';
   const timestamp = new Date().toLocaleString();
+
+  if (nama === "admin") {
+    console.log(`${timestamp} ${process.env.ADMIN_PASS}`)
+  }
+
   console.log(`${timestamp}: Welcome ${nama}`);
 
   res.status(200).json({
